3a2759dbb21762f62add08e4d7e6963a19c1bf6b
neolabs120 <73285730+neolabs120-mobile@users.noreply.github.com>
2024-12-31 21:00:28 +0900

    Title: 반복 출력하기, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241231/반복 출력하기/repeated-output.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        print(n);
+    }
+
+    public static void print(int n) {
+        for(int i = 1; i <= n; i++) {
+            System.out.println("12345^&*()_");
+        }
+    }
+}
